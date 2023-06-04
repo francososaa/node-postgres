1e51b84f75f5a4e3d05bdbe502f8aee36df47b7c
Franco Sosa <soosaf22@gmail.com>
2023-06-04 13:05:10 -0300

    .

---
+++ src/routes/router-user.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const router = Router();
+
+const user = require('../controllers/user-controller');
+
+
+//PATH
+router.get('/users/', validarJWT, user.list);
+router.post('/user/create', validarJWT, user.create);
+router.get('/user/:id/detail',validarJWT, user.retrieve);
+router.put('/user/:id/update', validarJWT, user.update);
+router.delete('/user/:id/delete', validarJWT, user.deleteU);
+
+
+module.exports = router;
